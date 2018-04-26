9505158fdf7a3a96abfd3cb56e7f13de6098ab8f
beyzabarut <beyzabrtt@gmail.com>
2018-04-26 08:34:02 +0300

    Post - Blog update

--- app/src/main/java/com/example/zeynep/cozcozdurapp/MainActivity.java
+++ app/src/main/java/com/example/zeynep/cozcozdurapp/MainActivity.java
@@ -59,7 +59,7 @@ public class MainActivity extends AppCompatActivity {
         sifreunutma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, YeniParolaActivity.class));
+       //         startActivity(new Intent(MainActivity.this, YeniParolaActivity.class));
             }
         });
 
--- app/src/main/java/com/example/zeynep/cozcozdurapp/PostActivity.java
+++ app/src/main/java/com/example/zeynep/cozcozdurapp/PostActivity.java
@@ -1,13 +1,91 @@
 package com.example.zeynep.cozcozdurapp;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity {
+    private ImageButton mSelectImage;
+    private EditText mPostTitle, mPostDesc;
+    private Button mSubmitBtn;
+    private Uri mImageUri = null;
+    private static final int GALLERY_REQUEST = 1;
+    private StorageReference mStorage;
+    private ProgressDialog mProgress;
+    private DatabaseReference mDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+        mSelectImage = (ImageButton)findViewById(R.id.imageSelect);
+        mPostTitle = (EditText)findViewById(R.id.titleField);
+        mPostDesc = (EditText)findViewById(R.id.descField);
+        mSubmitBtn = (Button)findViewById(R.id.submitBtn);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mProgress = new ProgressDialog(this);
+        mDatabase = FirebaseDatabase.getInstance().getReference().child("Blog");
+
+
+        mSelectImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+                galleryIntent.setType("image/*");
+                startActivityForResult(galleryIntent, GALLERY_REQUEST);
+            }
+        });
+
+        mSubmitBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startPosting();
+            }
+        });
+    }
+    private void startPosting() {
+        mProgress.setMessage("Posting to blog..");
+        mProgress.show();
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
+
+        if(!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mImageUri != null){
+            StorageReference filepath = mStorage.child("blog_images").child(mImageUri.getLastPathSegment());
+            filepath.putFile(mImageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUri = taskSnapshot.getDownloadUrl();
+                    DatabaseReference newPost = mDatabase.push();
+                    newPost.child("title").setValue(title_val);
+                    newPost.child("desc").setValue(desc_val);
+                    newPost.child("image").setValue(downloadUri.toString());
+                    mProgress.dismiss();
+                    startActivity(new Intent(PostActivity.this, CozCozdur.class));
+                }
+            });
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            mImageUri = data.getData();
+            mSelectImage.setImageURI(mImageUri);
+        }
     }
 }
